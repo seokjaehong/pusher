8f198eae68480baf1c4951fffb0dadc9cec9c0b2
seokjae hong <hsj2334@gmail.com>
2018-07-31 18:08:13 +0900

    progress에 따라 다른 색깔 progress bar 적용

--- app/pusher_test/views.py
+++ app/pusher_test/views.py
@@ -4,14 +4,10 @@ from pusher_test.tasks import send_pusher
 
 
 def pusher_test(request):
-    context = {}
     if request.method == 'GET':
         keyword = request.GET.get('keyword')
-        print(keyword)
+
         if keyword:
             send_pusher.delay(keyword)
-            # context = {
-            #     'results': keyword,
-            # }
         return render(request, 'base.html')
     return redirect('pusher-test')
